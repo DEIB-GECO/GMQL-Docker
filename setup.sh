c22ec488abe473e8a5164b55b92d93d1008301bc
lucananni93 <luca.nanni@polimi.it>
2019-03-27 20:17:08 +0100

    CI

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 spark_url="https://archive.apache.org/dist/spark/spark-2.2.0/spark-2.2.0-bin-hadoop2.7.tgz"
 gmql_docker_name="gmql"
 
+
 get_latest_release() {
 	echo "Downloading latest GMQL-WEB relase"
 	latest_release_url=$(curl --silent "https://api.github.com/repos/$1/releases/latest" | \
@@ -15,7 +16,7 @@ get_latest_release() {
 	file_name=$(echo $latest_release_url | awk -F"/" '{print $NF}')
 	echo "File name is $file_name"
 
-	unzip $file_name
+	unzip -qq $file_name
 	gmql_web_dir=$(echo "$file_name" | rev | cut -f 2- -d '.' | rev)
 	rm $file_name
 }
@@ -51,16 +52,30 @@ set_configurations() {
 	cat ${gmql_web_dir}/conf/gmql_conf/repository.xml
 }
 
+
+docker_login() {
+	echo "Logging in into DockerHub"
+	docker login --username=$DOCKERHUB_USERNAME --password=$DOCKERHUB_PASSWORD
+}
+
+
 build_docker() {
 	echo "Building GMQL docker"
-	docker build . -t $gmql_docker_name \
+	docker build . -t $DOCKERHUB_USERNAME/$gmql_docker_name \
 				--build-arg gmql_dir=./${gmql_web_dir}/ \
 				--build-arg spark_dir=./${spark_dir}/
+}
+
+
+push_docker() {
+	echo "Pushing docker image to DockerHub"
+	docker push $DOCKERHUB_USERNAME/$gmql_docker_name
 }
 
 
 get_latest_release "DEIB-GECO/GMQL-WEB"
 get_spark
 set_configurations
+docker_login
 build_docker
-
+push_docker
